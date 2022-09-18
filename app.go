1818e81ac53fa1525c75f8dd1d3471eea2e1eb31
Sebastian Olsson <sebelino7@gmail.com>
2022-09-18 22:07:07 +0200

    style: go fmt

--- app.go
+++ app.go
@@ -16,16 +16,16 @@ func main() {
 	}
 	dirMap := make(map[string]bool)
 	for _, file := range files {
-        dir := filepath.Dir(file)
+		dir := filepath.Dir(file)
 		dirMap[dir] = true
 	}
-    dirs := make([]string, len(dirMap))
-    i := 0
-    for d := range dirMap {
-        dirs[i] = d
-        i++
-    }
-    dirBytes, err := json.Marshal(dirs)
+	dirs := make([]string, len(dirMap))
+	i := 0
+	for d := range dirMap {
+		dirs[i] = d
+		i++
+	}
+	dirBytes, err := json.Marshal(dirs)
 
 	fmt.Println(string(dirBytes))
 }
